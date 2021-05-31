d607f327a326e97fb4dcae2545bb29a1fa9c9a1f
Jordan Kelly <profjjk@gmail.com>
2021-05-31 10:27:45 -0700

    create classes

--- M1-Challenge-Jordan-Kelly/CustomerComposition/src/main/java/com/company/App.java
+++ M1-Challenge-Jordan-Kelly/CustomerComposition/src/main/java/com/company/App.java
@@ -1,2 +1,13 @@
-package com.company;public class App {
+package com.company;
+
+public class App {
+    public static void main(String[] args) {
+        Customer jamesBond = new Customer("James", "Bond", "james@007.gov", "(007) 007-0070", true);
+        Address address = new Address("007 Secret Blvd.", "London", "England", "00700");
+
+        jamesBond.setShippingAddress(address);
+        jamesBond.setBillingAddress(address);
+
+        System.out.println(jamesBond.toString());
+    }
 }
--- M1-Challenge-Jordan-Kelly/CustomerComposition/src/main/java/com/company/Customer.java
+++ M1-Challenge-Jordan-Kelly/CustomerComposition/src/main/java/com/company/Customer.java
@@ -1,2 +1,122 @@
-package com.company;public class Customer {
+package com.company;
+
+import java.util.Objects;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private boolean rewardsMember;
+    private Address shippingAddress;
+    private Address billingAddress;
+
+    public Customer() {
+
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Customer(String firstName, String lastName, String email, String phoneNumber, boolean rewardsMember) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.rewardsMember = rewardsMember;
+    }
+
+    public Customer(String firstName, String lastName, String email, String phoneNumber, boolean rewardsMember, Address shippingAddress, Address billingAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.rewardsMember = rewardsMember;
+        this.shippingAddress = shippingAddress;
+        this.billingAddress = billingAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isRewardsMember() {
+        return rewardsMember;
+    }
+
+    public void setRewardsMember(boolean rewardsMember) {
+        this.rewardsMember = rewardsMember;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return rewardsMember == customer.rewardsMember && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName) && Objects.equals(email, customer.email) && Objects.equals(phoneNumber, customer.phoneNumber) && Objects.equals(shippingAddress, customer.shippingAddress) && Objects.equals(billingAddress, customer.billingAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, phoneNumber, rewardsMember, shippingAddress, billingAddress);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", rewardsMember=" + rewardsMember +
+                ", shippingAddress=" + shippingAddress +
+                ", billingAddress=" + billingAddress +
+                '}';
+    }
 }
