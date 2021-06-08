b57c920121b0571566c4f38c5cfac6152435d80c
Jordan Kelly <profjjk@gmail.com>
2021-06-07 17:58:04 -0700

    word of the day first attempt

--- M2-Challange-Jordan-Kelly/dailyword/src/main/java/com/company/model/Word.java
+++ M2-Challange-Jordan-Kelly/dailyword/src/main/java/com/company/model/Word.java
@@ -1,4 +1,67 @@
 package com.company.model;
 
+import java.util.Objects;
+
 public class Word {
+    // Instance variables
+    private int id;
+    private String word;
+    private String definition;
+
+    // Constructors
+    public Word() { }
+
+    public Word(int id, String word, String definition) {
+        this.id = id;
+        this.word = word;
+        this.definition = definition;
+    }
+
+    // Getters & setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    // Overrides
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return id == word1.id && Objects.equals(word, word1.word) && Objects.equals(definition, word1.definition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, word, definition);
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "id=" + id +
+                ", word='" + word + '\'' +
+                ", definition='" + definition + '\'' +
+                '}';
+    }
 }
